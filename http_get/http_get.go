7e359cd056e44b35d72f2d5fb236325a8f522420
Tomokazu Karube <karubetomokazu@gmail.com>
2018-11-10 20:25:57 +0000

    modify http_get.go

--- http_get/http_get.go
+++ http_get/http_get.go
@@ -1,11 +1,13 @@
+// program_name <IP address of VS>
+
 package main
 
 import (
-	"fmt"
-	"net/http"
-)
+        "fmt"
+        "net/http"
+       )
 
 func main() {
-	resp, _ := http.Get("http://10.0.11.138")
-	fmt.Println(resp)
+    resp, _ := http.Get("http://10.0.11.138")
+    fmt.Println(resp)
 }
